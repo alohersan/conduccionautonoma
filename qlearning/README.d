62e2b0529a11abfb93a33a00ffc37c5a3de492a7
Alondra <alondrahernandez111@gmail.com>
2025-07-12 21:20:32 +0100

    Create README.d

---
+++ qlearning/README.d
@@ -0,0 +1,2 @@
+# Q-learning
+Para este proyecto, Q-Learning se aplicó en el entorno Taxi-v3 de Gymnasium, una simulación en la que un taxi debe recoger y dejar pasajeros en un hotel, siendo cada ubicación una cuadrícula.
